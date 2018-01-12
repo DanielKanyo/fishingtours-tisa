21f212e530481729b4475fc52864ab9975b6527d
DanielKanyo <danielkanyo992@gmail.com>
2018-01-12 23:31:13 +0100

    menu is now in app.js, 3 new picture, content and side bar with background shadow

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,46 +1,19 @@
 import React, { Component } from 'react';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
-
+/** style */
 import './Home.css';
-
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 class Home extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      index: 0
-    }
+    this.state = {}
   }
 
-  handleChangeIndex = index => {
-    this.setState({
-      index,
-    });
-  };
-
   render() {
-    const { index } = this.state;
     return (
-      <div className="Home" >
-        <div className="homeHeader" >
-          <div className="swipeableView" >
-            <AutoPlaySwipeableViews index={index} onChangeIndex={this.handleChangeIndex}>
-              <div className="swipeView first">
-                {this.props.objectProp.data.title}
-              </div>
-              <div className="swipeView second">
-                asd
-              </div>
-              <div className="swipeView third">
-                asd
-              </div>
-            </AutoPlaySwipeableViews>
-          </div>
-        </div>
-
+      <div className="Home">
+        <div className="Content">Content</div>
+        <div className="Sidebar">Sidebar</div>
       </div>
     );
   }
