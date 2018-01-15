cb117b64e126091f0c2b462b88215de42dfe528e
DanielKanyo <danielkanyog@gmail.com>
2018-01-15 13:32:18 +0100

    facebook message and like

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -14,8 +14,21 @@ class Sidebar extends Component {
       <div className="Sidebar">
         <div className="sidebarTitle">Sidebar</div>
         <div className="sidebarContent">
-          Content  
-        </div> 
+          <div className="fb-page"
+            data-href="https://www.facebook.com/KanyoJanos65/"
+            data-tabs="messages"
+            data-small-header="true"
+            data-adapt-container-width="false"
+            data-hide-cover="true"
+            data-show-facepile="true">
+            <blockquote cite="https://www.facebook.com/KanyoJanos65/"
+              className="fb-xfbml-parse-ignore">
+              <a href="https://www.facebook.com/KanyoJanos65/">
+                Horgásztúrák - Magyarkanizsa
+          </a>
+            </blockquote>
+          </div>
+        </div>
       </div>
     );
   }
